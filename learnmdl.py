2c91a1ac6ebe9b57d4745204cc0a3400d3755d62
Ubuntu <ubuntu@ip-172-31-43-55.eu-west-1.compute.internal>
2017-06-09 19:49:50 +0000

    model learning and saving

--- learnmdl.py
+++ learnmdl.py
@@ -4,7 +4,7 @@ import re
 import sys
 import logging
 
-import ngramiter
+import ngram
 
 from gensim.models import Word2Vec
 
@@ -13,8 +13,8 @@ modelFile = sys.argv[2]
 
 logging.basicConfig(level = logging.INFO)
 
-lines = ngramiter.CorpusNgramIterator(dataFile)
+lines = ngram.SentenceNgramSampler(dataFile)
 
-model = Word2Vec(lines, workers=16, sg=1, hs=1)
+model = Word2Vec(lines, workers=16, sg=1, hs=1, iter=25)
 
 model.wv.save_word2vec_format(modelFile, binary = True)
