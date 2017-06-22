6de6c0d24ab1f7eddc665b967165d03332717538
Ubuntu <ubuntu@ip-172-31-43-55.eu-west-1.compute.internal>
2017-06-22 15:38:50 +0000

    partial upd

--- learnmdl.py
+++ learnmdl.py
@@ -8,13 +8,25 @@ import ngram
 
 from gensim.models import Word2Vec
 
-dataFile = sys.argv[1]
-modelFile = sys.argv[2]
+#raw / use factor X
+#filter freqs
+#filter factors
 
-logging.basicConfig(level = logging.INFO)
+if __name__ == "__main__":
+	dataFile = sys.argv[1]
+	modelFile = sys.argv[2]
+	
+	srcFactor = None
+	freqFilter = [5, 30, 50]
+	firstPosFilter = None
+	lastPosFilter = None
+	somePosFilter = None
+	beta = 0.125
+	
+	logging.basicConfig(level = logging.INFO)
 
-lines = ngram.SentenceNgramSampler(dataFile, minCounts = [None, 30, 40, 50])
+	lines = ngram.SentenceNgramSampler(dataFile, minCounts = freqFilter, srcFactor = srcFactor, firstPosFilter = firstPosFilter, lastPosFilter = lastPosFilter, atLeastOnePosFilter = somePosFilter, ngramThresholdBeta = beta)
 
-model = Word2Vec(lines, workers=16, sg=1, hs=1, iter=10)
+	model = Word2Vec(lines, workers=20, sg=1, hs=1, iter=10)
 
-model.wv.save_word2vec_format(modelFile, binary = True)
+	model.wv.save_word2vec_format(modelFile, binary = True)
--- ngram.py
+++ ngram.py
@@ -19,47 +19,69 @@ class SentenceNgramSampler:
 	
 	ngramDict = defaultdict(lambda: defaultdict(int))
 	
-	data = []
+	storedData = []
 	
 	currSntIdx = 0
 
-	def __init__(self, filename, minCounts = [5, 50, 80], ngramInclThreshold = 0.5):
-		# read corpus once to fill the ngram dict
+	def __init__(self, filename, minCounts = [None, 30, 50], ngramThresholdBeta = 0.125,
+				firstPosFilter = None, lastPosFilter = None, atLeastOnePosFilter = None,
+				crazyBigCorpus = False):
+		
 		self.maxNgramLen = len(minCounts)
 		self.ngramInclThreshold = ngramInclThreshold
 		self.minCounts = minCounts
 		
-		self._readData(filename)
+		self.fileHandle = open(filename, 'r')
 		
-		self._filterNgrams(minCounts)
+		#self._readData(filename)
+		
+		#self._filterNgrams(minCounts)
 	
-	def _readData(self, filename):
-		logger.info("Reading data")
+	def __next__(self):
+		try:
+			srcSnt = self.data[self.currSntIdx]
+		except IndexError:
+			self.currSntIdx = 0
+			raise StopIteration
 		
-		with open(filename, 'r') as fh:
-			idx = 0
-			
-			for rawline in fh:
-				sentence = [t for t in rawline.strip().lower().split() if re.search(r'\w', t)]
-				
-				self.data.append(sentence)
-				
-				for ngram, spec in self.ngrams(sentence):
-					self.ngramDict[len(spec) - 1][ngram] += 1
-				
-				idx += 1
-				
-				if not idx % 500000:
-					logger.info("Read {0} sentences".format(idx))
+		ngramsAndSpecs = list(self.ngrams(srcSnt))
+		random.shuffle(ngramsAndSpecs)
+		
+		toJoin = self._getNonoverlappingNgrams(ngramsAndSpecs)
+		
+		result = self._applyJoinOps(srcSnt, toJoin)
+		
+		self.currSntIdx += 1
+		
+		return result
 	
-	def _filterNgrams(self, minCounts):
-		logger.info("Filtering n-grams")
-		
-		for nlen in range(1, self.maxNgramLen):
-			before = len(self.ngramDict[nlen])
-			self.ngramDict[nlen] = { k: v for k, v in self.ngramDict[nlen].items() if v > minCounts[nlen] }
-			after = len(self.ngramDict[nlen])
-			logger.info("Filtered n-grams of length {0} from {1} down to {2}".format(nlen + 1, before, after))
+	#def _readData(self, filename):
+	#	logger.info("Reading data")
+	#	
+	#	with open(filename, 'r') as fh:
+	#		idx = 0
+	#		
+	#		for rawline in fh:
+	#			sentence = [t for t in rawline.strip().lower().split() if re.search(r'\w', t)]
+	#			
+	#			self.data.append(sentence)
+	#			
+	#			for ngram, spec in self.ngrams(sentence):
+	#				self.ngramDict[len(spec) - 1][ngram] += 1
+	#			
+	#			idx += 1
+	#			
+	#			if not idx % 500000:
+	#				logger.info("Read {0} sentences".format(idx))
+	
+	#def _filterNgrams(self, minCounts):
+	#	logger.info("Filtering n-grams")
+	#	
+	#	for nlen in range(1, self.maxNgramLen):
+	#		before = len(self.ngramDict[nlen])
+	#		self.ngramDict[nlen] = { k: v for k, v in self.ngramDict[nlen].items() if v > minCounts[nlen] }
+	#		after = len(self.ngramDict[nlen])
+	#		logger.info("Filtered n-grams of length {0} from {1} down to {2}".format(nlen + 1, before, after))
 	
 	def ngrams(self, srcSnt):
 		for idx, tok in enumerate(srcSnt):
@@ -92,24 +114,6 @@ class SentenceNgramSampler:
 		
 		for op in sorted(toJoin, key=lambda x: -min(x)):
 			result = result[:min(op)] + ["__".join([sentence[i] for i in sorted(op)])] + result[max(op)+1:]
-		
-		return result
-	
-	def __next__(self):
-		try:
-			srcSnt = self.data[self.currSntIdx]
-		except IndexError:
-			self.currSntIdx = 0
-			raise StopIteration
-		
-		ngramsAndSpecs = list(self.ngrams(srcSnt))
-		random.shuffle(ngramsAndSpecs)
-		
-		toJoin = self._getNonoverlappingNgrams(ngramsAndSpecs)
-		
-		result = self._applyJoinOps(srcSnt, toJoin)
-		
-		self.currSntIdx += 1
 		
 		return result
 	
