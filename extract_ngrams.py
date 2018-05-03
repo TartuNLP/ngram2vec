956db10ba988994d6961cfe7ceb4f4768c45dd98
Andre Tättar <b02166@ut.ee>
2018-05-03 18:08:01 +0300

    Update extract_ngrams.py

--- extract_ngrams.py
+++ extract_ngrams.py
@@ -1,18 +1,10 @@
-import re
 import sys
-import logging
 
 import ngram
-
-from datetime import datetime
-
-def debug(msg):
-	sys.stderr.write("{0}: {1}\n".format(str(datetime.now()), msg))
 
 if __name__ == "__main__":
 	dataFile = sys.argv[1]
 	outFile = sys.argv[2]
-	logging.basicConfig(level = logging.INFO)
 	beta = 0.125
 	freqFilter = [20,120,90]
 
@@ -21,3 +13,4 @@ if __name__ == "__main__":
 	with open(outFile, "w") as f:
 		for i in lines:
 			f.write(" ".join(i) + "\n")
+	print("Job finished")
